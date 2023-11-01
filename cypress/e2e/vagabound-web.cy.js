a9ae3131e1519cf88c1f01a5752edf18f80bf0fd
Felipe Crespo Gambade <felipecrespo@pm.me>
2023-11-01 17:20:05 +0100

    testing

--- cypress/e2e/vagabound-web.cy.js
+++ cypress/e2e/vagabound-web.cy.js
@@ -1,7 +1,8 @@
 describe('template spec', () => {
   it('passes', () => {
     cy.visit('https://app.lambus.com/');
-    cy.get('#input-56').type('test-');
-    cy.get('#input-56').type(Cypress.env('LAMBUS_PASSWORD'));
+    cy.get('#input-56').type(Cypress.env('LAMBUS_USERNAME'));
+    cy.get('#input-61').type(Cypress.env('LAMBUS_PASSWORD'));
+    cy.get('.v-form > .l-btn > .v-btn__content').click();
     })
 })
