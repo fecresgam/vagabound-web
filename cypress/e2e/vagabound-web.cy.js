6818ae42c26ac13c4c33063085a9829a31c92aca
Felipe Crespo Gambade <felipecrespo@pm.me>
2023-11-01 16:25:42 +0100

    Creating cypress.env.json not on repo

--- cypress/e2e/vagabound-web.cy.js
+++ cypress/e2e/vagabound-web.cy.js
@@ -2,6 +2,6 @@ describe('template spec', () => {
   it('passes', () => {
     cy.visit('https://app.lambus.com/');
     cy.get('#input-56').type('test-');
-    cy.get('#input-56').type('secret');
+    cy.get('#input-56').type(Cypress.env('LAMBUS_PASSWORD'));
     })
 })
